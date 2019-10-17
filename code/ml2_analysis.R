f7cefa98fea5528b97d91694f6a38a624de82081
raklein <raklein22@gmail.com>
2019-10-17 12:53:24 +0200

    Add random forest Tidy up formatting and comments

--- code/ml2_analysis.R
+++ code/ml2_analysis.R
@@ -1,7 +1,9 @@
 ### Corresponding coder: Rick Klein raklein22@gmail.com 
-library(randomForest)
-library(party)
-library(tidyverse)
+library(randomForest) # for crf
+library(party) # for crf
+library(tidyverse) # convenience tools for data handling
+library(ranger) # for rf
+library(caret) # for rf
 
 # setwd() if not using the .rproj file
 
@@ -12,18 +14,16 @@ set.seed(1)
 # load the ml2 slate 1 data. Using the not-deidentified datasets
 data <- readRDS("./data/ML2_RawData_S1.rds")
 
-# # dropping some irrelevant columns. CRF has problems handling missing data.
-# data <- data %>%
-#   select(StartDate:education, Source.Global:IDiffOrderN)
-# 
-# # For the moment I'm going restrictively select only a few vars
-# data <- data %>%
-#   select(subjwell, tipi_1:tipi_10, mood)
+################################################################################
+#### Data preparation ##########################################################
+################################################################################
 
-# Select all numeric variables
+# Select only numeric variables
 data <- select_if(data, is.numeric)
 
-# code the IMC pass/fail so we don't lose it in the next step.
+# Code the Instructional Manipulation Check/attention check as pass/fail
+# so we don't lose it in the next step.
+# Participants pass the check if they check all four boxes.
 data <- data %>% 
   rowwise() %>% 
   mutate(imc_sum = sum(c(IMC1_1, IMC1_2, IMC1_3, IMC1_4), na.rm = TRUE))
@@ -31,8 +31,12 @@ data <- data %>%
 data$imc_pass <- 0
 data$imc_pass[data$imc_sum == 4] <- 1
 
-# Let's drop any variable with more than X percent missing values
-# These are typically site-specific variables (custom consent pages, etc.)
+# Major problem is that CRF doesn't play well with missing data (afaik)
+# Next few steps try to counteract this.
+
+# Drop any variable with more than X percent missing values
+# Many of these are site-specific variables (custom consent pages, etc.)
+# with data only from a few sites.
 data <- data[, -which(colMeans(is.na(data)) > 0.15)]
 
 # Still some junk variables remaining, deleting them:
@@ -49,15 +53,74 @@ data <- data %>%
 # drop rows with missing data in any column
 data <- data[complete.cases(data),]
 
-# split into training and test (50/50 split)
+################################################################################
+#### Data split ################################################################
+################################################################################
+
+# split into training, validation, and test datasets. (50/25/25 split)
+# the 'training' dataset is used to build the models. Their predictive accuracy
+# out-of-sample is assessed using the 'validation' dataset, after which
+# we can tune the model for better prediction. The 'test' dataset is reserved
+# as a final unbiased test of performance of the final model (one time, in theory).
 rows <- sample(nrow(data)) #creates randomized vector same length as data
 data_randomized <- data[rows,] #randomizes df to index from 'rows'
-split <- round(nrow(data)*.50) #creates index to split the file into 2/3 1/3, rounded
-train <- data_randomized[1:split,] #first 2/3 to train
-test <- data_randomized[(split+1):nrow(data),] #remaining 1/3 to test
+split <- floor(nrow(data)*.50) #creates index to split the file at 50% of data, rounded
+split2 <- floor(nrow(data)*.75) #creates index to split the file at 75% of data, rounded
+train <- data_randomized[1:split,] #first 1/2 to train
+validation <- data_randomized[(split+1):split2,] # next 1/4 to validation
+test <- data_randomized[(split2+1):nrow(data),] #remaining 1/4 to test
 
-# fit the conditional forest
-forest_model <- cforest(subjwell ~ ., 
+################################################################################
+#### Linear Regression #########################################################
+################################################################################
+
+# Running a multiple linear regression as we normally might in the course of 
+# research. Predicting the subjective wellbeing DV from all other 
+# variables in the dataset.
+lm_model <- (lm(data = train, subjwell ~ .)) # the period indicates to use all other vars in the dataframe
+
+# Typically in psych we would look at these results and call it a day:
+# summary(lm_model)
+
+# But here we'll test these predictions "out-of-sample". We'll take the 
+# model generated on the training data, and make predictions for each observation
+# in the 'validation' dataset that the algorythm hasn't seen yet. This is
+# prediction, instead of explanation.
+lm_prediction_train <- predict(lm_model) 
+lm_prediction_validation <- predict(lm_model, newdata = validation) 
+
+# Evaluate in a common metric
+lm_performance_train <- cor(train$subjwell, lm_prediction_train)^2
+lm_performance_validation <- cor(validation$subjwell, lm_prediction_validation)^2
+
+
+################################################################################
+#### Random Forest #############################################################
+################################################################################
+
+# Next we'll fit a basic machine learning model - Random Forests. 
+rf_model <- train(
+  subjwell ~ ., 
+  tuneLength = 1, 
+  data = train, method = "ranger", 
+  trControl = trainControl(method = "cv", number = 5, verboseIter = TRUE)
+)
+
+# Extract predictions
+rf_prediction_train <- predict(rf_model) #oob
+rf_prediction_validation <- predict(rf_model, newdata = validation)
+
+# Evaluate
+rf_performance_train <- cor(train$subjwell, rf_prediction_train)^2
+rf_performance_validation <- cor(validation$subjwell, rf_prediction_validation)^2
+
+################################################################################
+#### Conditional Random Forest #################################################
+################################################################################
+
+# Next we try a conditional random forest model, which is RF but with 
+# some added properties.
+crf_model <- cforest(subjwell ~ ., 
                         data = train, 
                         controls = cforest_control(teststat = "quad", 
                                                    testtype = "Univ", 
@@ -68,26 +131,25 @@ forest_model <- cforest(subjwell ~ .,
                                                    fraction = 0.632))
 
 # Extract predictions
-model_prediction_train <- predict(forest_model) #oob
-model_prediction_test <- predict(forest_model, newdata = test)
+crf_prediction_train <- predict(crf_model) #oob
+crf_prediction_validation <- predict(crf_model, newdata = validation)
 
 # Evaluate
-cor(train$subjwell, model_prediction_train)^2
-cor(test$subjwell, model_prediction_test)^2
+crf_performance_train <- as.numeric(cor(train$subjwell, crf_prediction_train)^2)
+crf_performance_validation <- as.numeric(cor(validation$subjwell, crf_prediction_validation)^2)
 
-# compare with a regular regression
-lm_model <- (lm(data = train, subjwell ~ .)) # the period indicates to use all other vars in the dataframe
+# Compare results from all three models
 
-# Typically in psych we would look at these results and call it a day:
-# summary(lm_model)
+compare_results <- data.frame(lm_performance_train,
+                                lm_performance_validation,
+                                rf_performance_train,
+                                rf_performance_validation,
+                                crf_performance_train,
+                                crf_performance_validation)
 
-# but here we'll again test these predictions in data the 'algorythm' hasn't seen yet
-model_prediction_lm_train <- predict(lm_model) 
-model_prediction_lm_test <- predict(lm_model, newdata = test) 
-
-# evaluate in a common metric like before
-cor(train$subjwell, model_prediction_lm_train)^2
-cor(test$subjwell, model_prediction_lm_test)^2
-
-
-
+names(compare_results) <- c("Linear Regression R^2 - Training",
+                            "Linear Regression R^2 - Validation",
+                            "Random Forest R^2 - Training",
+                            "Random Forest R^2 - Validation",
+                            "Conditional RF R^2 - Training",
+                            "Conditional RF R^2 - Validation")
